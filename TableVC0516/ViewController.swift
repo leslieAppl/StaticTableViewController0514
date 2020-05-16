ac135aa6cdb3a061b50388759684f9a34dbd7aaf
Leslie <leslie.appl@gmail.com>
2020-05-16 14:59:37 +0800

    Initial Commit

---
+++ TableVC0516/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableVC0516
+//
+//  Created by leslie on 5/16/20.
+//  Copyright © 2020 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
